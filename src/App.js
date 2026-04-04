fc0d4e8cb6ed3b9d04ca8d3240c8af975868014f
Vaibhav1506 <bvaibhav1506@gmail.com>
2026-04-04 19:41:11 +0530

    Fixed routing for GitHub Pages (basename added)

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import About from './pages/About';
-import Reports from './pages/Reports';
-import Navbar from './components/Navbar';
-import "./styles/App.css";
+  import React, {useState, useEffect} from 'react';
+  import {BrowserRouter, Routes, Route} from 'react-router-dom';
+  import Dashboard from './pages/Dashboard';
+  import About from './pages/About';
+  import Reports from './pages/Reports';
+  import Navbar from './components/Navbar';
+  import "./styles/App.css";
 
-function App() {
-  const [transactions, setTransactions] = useState(() => {
-    const storedData = localStorage.getItem("transactions");
-    return storedData ? JSON.parse(storedData) : [];
-  });
+  function App() {
+    const [transactions, setTransactions] = useState(() => {
+      const storedData = localStorage.getItem("transactions");
+      return storedData ? JSON.parse(storedData) : [];
+    });
 
-  useEffect(() => {
-    localStorage.setItem("transactions", JSON.stringify(transactions));
-  }, [transactions]);
+    useEffect(() => {
+      localStorage.setItem("transactions", JSON.stringify(transactions));
+    }, [transactions]);
 
-  return (
-    <div className = "app-wrapper">
-    <BrowserRouter>
-        <Navbar/>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Dashboard transactions = {transactions} setTransactions = {setTransactions}/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/reports" element={<Reports transactions = {transactions}/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
-      <footer className="footer">
-        <div className="footer-content">
-          <p>Contact</p>
-
-          <div className="footer-links">
-            <a href="https://github.com/Vaibhav1506">
-              GitHub
-            </a>
-            <a href="mailto:bvaibhav1506@gmail.com">
-              Email
-            </a>
-            <a href="https://www.linkedin.com/in/vaibhav-bakshi-8b896832a/">
-              LinkedIn
-            </a>
-          </div>
-
+    return (
+      <div className = "app-wrapper">
+      <BrowserRouter basename="/Expense-Tracker-React-JS-Demo">
+          <Navbar/>
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Dashboard transactions = {transactions} setTransactions = {setTransactions}/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/reports" element={<Reports transactions = {transactions}/>}/>
+          </Routes>
         </div>
-      </footer>
-    </div>
-  );
-}
+      </BrowserRouter>
+        <footer className="footer">
+          <div className="footer-content">
+            <p>Contact</p>
 
-export default App;
+            <div className="footer-links">
+              <a href="https://github.com/Vaibhav1506">
+                GitHub
+              </a>
+              <a href="mailto:bvaibhav1506@gmail.com">
+                Email
+              </a>
+              <a href="https://www.linkedin.com/in/vaibhav-bakshi-8b896832a/">
+                LinkedIn
+              </a>
+            </div>
+
+          </div>
+        </footer>
+      </div>
+    );
+  }
+
+  export default App;
